9eee783eaf50043806c4e38d2e068a706da6b0a9
Tachera Sasi <tacherasasi@gmail.com>
2025-11-28 13:09:46 +0300

    Replace gliderlabs/ssh with wish and bubbletea
    Use wish's Bubble Tea middleware to run a TUI per SSH session. Add a
    simple counter demo, graceful shutdown, and host key configuration;
    update dependencies in go.mod/go.sum.

--- main.go
+++ main.go
@@ -1,15 +1,86 @@
- package main
+package main
 
- import (
-     "github.com/gliderlabs/ssh"
-     "io"
-     "log"
- )
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
- func main() {
-     ssh.Handle(func(s ssh.Session) {
-         io.WriteString(s, "Hello world\n")
-     })  
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/ssh"
+	"github.com/charmbracelet/wish"
+	bm "github.com/charmbracelet/wish/bubbletea"
+)
 
-     log.Fatal(ssh.ListenAndServe(":2222", nil))
- }
+type model struct {
+	counter int
+}
+
+func (m model) Init() tea.Cmd { return nil }
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q", "ctrl+c":
+			return m, tea.Quit
+		case "up":
+			m.counter++
+		case "down":
+			m.counter--
+		}
+	}
+	return m, nil
+}
+
+func (m model) View() string {
+	return fmt.Sprintf(`
+SSH TUI Demo
+
+Press up or down to change the counter
+Counter: %d
+
+Press q to quit
+`, m.counter)
+}
+
+// You need a handler that returns the initial model for each new SSH session
+func teaHandler(s ssh.Session) (tea.Model, []tea.ProgramOption) {
+	return model{}, []tea.ProgramOption{tea.WithAltScreen()}
+}
+
+func main() {
+	// 1. Create the Wish SSH Server
+	s, err := wish.NewServer(
+		wish.WithAddress(":2222"),
+		wish.WithHostKeyPath(".ssh/id_ed25519"),
+		wish.WithMiddleware(
+			bm.Middleware(teaHandler), // This bridges SSH to Bubble Tea
+		),
+	)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// 2. Handle graceful shutdowns
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	log.Println("SSH server started on localhost:2222")
+
+	go func() {
+		if err = s.ListenAndServe(); err != nil && err != ssh.ErrServerClosed {
+			log.Fatalln(err)
+		}
+	}()
+
+	<-done
+	log.Println("Stopping SSH server...")
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer func() { cancel() }()
+	if err := s.Shutdown(ctx); err != nil {
+		log.Fatalln(err)
+	}
+}
